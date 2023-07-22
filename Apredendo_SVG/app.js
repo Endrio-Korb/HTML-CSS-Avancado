f962e33b1a174d1eb1fa07bc619e79ba4ee2c69f
Endrio-Korb <alexkorb95@gmail.com>
2023-07-22 13:39:24 -0300

    .

---
+++ Apredendo_SVG/app.js
@@ -0,0 +1,12 @@
+// const circle = document.querySelector('.meuCirculo')
+// let r = 30;
+
+// circle.addEventListener('click', () => {
+//     r += 10
+//     circle.style.r = r
+// })
+
+// circle.addEventListener('mouseout', () => {
+//     circle.style.r = 30
+//     r = 30
+// })
